43ac42a8b6ef0ff5234ebead107f634864bfa9b5
bansalr323 <bansalr@brynmawrschool.org>
2025-06-17 10:43:56 -0400

    add new file

---
+++ calculator.py
@@ -0,0 +1,27 @@
+num1 = 1
+num2 = 2 
+sum = num1 + num2
+print("Sum:")
+print(sum)
+
+num3 = 3 
+num4 = 4 
+multiplication = num3 * num4 
+print("Multiplication:")
+print(multiplication)
+num5 = 5 
+num6 = 6
+division = num5/num6 
+print("Division:")
+print(division)
+num7 = 7 
+num8 = 8 
+difference = num8 - num7 
+print("Difference:")
+print(difference) 
+num9 = 9
+num10 = 10
+print("Modular Division:")
+modulus = 10%9
+print(modulus)
+
---
+++ test.py
@@ -0,0 +1,21 @@
+print("Hello World") 
+print(5+10)
+print("This is a string")
+print(5*10)
+print(10/5)
+print("Printing number:")
+number = 10 
+print(number)
+num2 = 5 
+print("Sum of number and num2:")
+print(number + num2)
+sum = number + num2 
+print("Sum variable:")
+print(sum)
+name = "Ria Bansal"
+print("Printing name:")
+print(name)
+number = 10 
+print(number)
+number = 25 
+print(number)
