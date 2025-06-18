1bbe9c8ce6907fabbcbf90b9a7a17fbedf2e31d2
bansalr323 <bansalr@brynmawrschool.org>
2025-06-18 12:00:36 -0400

    Update studentid.py

--- studentid.py
+++ studentid.py
@@ -1,4 +1,4 @@
 name = input("Enter your name: ")
-gradelevel = input("Enter your grade level: ") 
+gradelevel = int(input("Enter your grade level: "))
 email = input("Enter your email: ") 
 print(f"Hello, {name}. Your grade level is {gradelevel} and your email is {email}.") 
