9a2319d6dfe43f6884c4a65436bc6e4aa71cb7f5
bansalr323 <bansalr@brynmawrschool.org>
2025-06-23 21:07:57 -0400

    Homework

--- mushrooms.py
+++ mushrooms.py
@@ -2,20 +2,32 @@ mushrooms = []
 check = True 
 
 while check == True: 
-    option = int(input("Enter mushroom weight, or STOP to end: "))
+    option = input("Enter mushroom weight, or STOP to end: ")
     if option == "STOP": 
-        check = False 
-        for mushroom in mushrooms: 
-            if mushroom >100: 
-                mushroom = large_mushroom 
-            elif mushroom <100: 
-                mushroom = small_mushroom 
-            else: 
-                mushroom = medium_mushroom  
-    elif option < 1: 
+        check = False  
+    elif int(option) < 1: 
         print("Weight must be bigger than zero!") 
     else: 
-        mushrooms.append(option)
+        mushrooms.append(int(option))
+
+big_mushroom = 0 
+small_mushroom = 0 
+medium_mushroom = 0 
+
+for size in mushrooms: 
+    if size > 1000: 
+        big_mushroom += 1 
+
+    elif size < 100: 
+        small_mushroom += 1 
+
+    else:
+        medium_mushroom += 1 
+print(f"There were {small_mushroom} small mushrooms, {medium_mushroom} medium mushrooms, and {big_mushroom} large mushrooms.")
+
+        
+
+
 
 
 
